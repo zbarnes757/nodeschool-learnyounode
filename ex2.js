6eb7ef27e9a6fbebe8b9214c0c6179a230676a5f
Zac Barnes <zac.barnes89@gmail.com>
2015-11-24 13:51:46 -0600

    add instructions where I forgot them

--- ex2.js
+++ ex2.js
@@ -1,3 +1,8 @@
+// ## BABY STEPS (Exercise 2 of 13)
+//
+//  Write a program that accepts one or more numbers as command-line arguments
+//  and prints the sum of those numbers to the console (stdout).
+
 var input = process.argv;
 var total = 0;
 for (var i in input) {
